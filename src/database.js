b41fb880d04eb098a2390e32bc4dd08f8a2aff0c
SilvaEmerson <emersonjose877@gmail.com>
2021-02-24 01:35:52 +0000

    Update tests: use sqlite on test environment

--- src/database.js
+++ src/database.js
@@ -2,19 +2,26 @@ const { Sequelize } = require("sequelize");
 const { Parking } = require("./models/parking");
 
 const loadDatabase = () => {
+  const dbSettings =
+    process.env.API_ENV === "TEST"
+      ? { dialect: "sqlite", host: "../database.sqlite" }
+      : {
+          dialect: "postgres",
+          host: process.env.DB_HOST,
+          password: process.env.POSTGRES_PASSWORD,
+          username: "postgres",
+          port: 5432,
+        };
+
   const db = new Sequelize({
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    password: process.env.POSTGRES_PASSWORD,
-    username: "postgres",
-    port: 5432,
+    ...dbSettings,
     logging: false,
   });
 
   return db;
 };
 
-const db = loadDatabase()
+const db = loadDatabase();
 const parking = Parking(db);
 
 module.exports = {
