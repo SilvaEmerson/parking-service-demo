4053c573b458fab1f42785fb92fd0020537fbf07
SilvaEmerson <emersonjose877@gmail.com>
2021-02-24 02:31:15 +0000

    Update database loading

--- src/models/parking.js
+++ src/models/parking.js
@@ -1,40 +1,46 @@
 const { DataTypes } = require("sequelize");
 
 const Parking = (sequelize) =>
-  sequelize.define("parking", {
-    // Model attributes are defined here
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+  sequelize.define(
+    "parking",
+    {
+      // Model attributes are defined here
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      plate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      paid: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      left: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      leftAt: {
+        type: DataTypes.DATE,
+      },
+      enteredAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      time: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    plate: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    paid: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-    },
-    left: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-    },
-    leftAt: {
-      type: DataTypes.DATE,
-    },
-    enteredAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    time: {
-      type: DataTypes.STRING,
-      allowNull: true
+    {
+      freezeTableName: true,
     }
-  });
+  );
 
 module.exports = {
   Parking,
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.API_PORT || 3000;
 
 const start = async () => {
   await db.authenticate();
+  await db.sync();
   app.listen(PORT, () => {
     console.log(`API running at http://localhost:${PORT}`);
   });
